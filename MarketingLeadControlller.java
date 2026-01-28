70d020442f2d418388b2e5b980fc8167bb90021f
shailesh56789 <shaishailesh00@gmail.com>
2026-01-28 18:16:09 +0530

    Update MarketingLeadControlller.java

--- MarketingLeadControlller.java
+++ MarketingLeadControlller.java
@@ -2,13 +2,18 @@ package com.Kairali.marketing.leads.controller;
 
 import com.Kairali.marketing.leads.domain.MarketingLead;
 import com.Kairali.marketing.leads.service.MarketingLeadService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class MarketingLeadControlller {
 
     private MarketingLeadService marketingLeadService;
@@ -18,9 +23,16 @@ public class MarketingLeadControlller {
     }
 
     @PostMapping("/SaveAll")
-    public ResponseEntity<?> SaveAllMarketingData(List<MarketingLead> marketingList)
+    public ResponseEntity<?> SaveAllMarketingData(  @RequestBody List<MarketingLead> marketingList)
     {
-        return marketingLeadService.saveMarketingLeads(marketingList);
+         marketingLeadService.saveMarketingLeads(marketingList);
+        return new ResponseEntity<>("Marketing data saved successfully", HttpStatus.OK);
+    }
+    @PostMapping("/SaveAll1")
+    public ResponseEntity<?> SaveAllMarketingData1(  @RequestBody List<MarketingLead> marketingList)
+    {
+        marketingLeadService.saveMarketingLeads1(marketingList);
+        return new ResponseEntity<>("Marketing data saved successfully", HttpStatus.OK);
     }
 
 }
